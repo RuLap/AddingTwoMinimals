1643fb4c8660982cfcda9d348fbfec2a0730e017
Igor Meshkov <meshkov-74@yandex.ru>
2021-05-16 01:49:56 +0300

    Add project files.

---
+++ AddingTwoMinimals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddingTwoMinimals
+{
+    class Program
+    {
+        const int MaxArrayLen = 10000;
+        static void Main(string[] args)
+        {
+        }
+
+        public static int GetMinimalsSum()
+        {
+            return 0;
+        }
+    }
+}
